d6fecf717aa061c53261f5e21703835bb109e62b
pizzasahu <199201shubhamsahu@gmail.com>
2018-10-30 02:25:27 +0530

    Added Colours Chapter

--- app/src/main/java/com/example/root/deutsche/ColoursActivity.kt
+++ app/src/main/java/com/example/root/deutsche/ColoursActivity.kt
@@ -1,62 +1,67 @@
 package com.example.root.deutsche
 
 import android.content.Intent
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
-import com.google.firebase.auth.FirebaseAuth
+import android.os.Bundle
 import android.support.design.widget.NavigationView
-import com.google.firebase.auth.FirebaseAuth.AuthStateListener
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.MenuItem
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_colours.*
 
-class User{
-    var name: String =""
-    var email: String =""
-    var marks: Int = 0
+class Colours{
+
+    var englishColour: String =""
+    var germanColour: String =""
 }
 
-class MainActivity : NavigationView.OnNavigationItemSelectedListener, AppCompatActivity () {
-
+class ColoursActivity: NavigationView.OnNavigationItemSelectedListener, AppCompatActivity () {//TODO 3
 
     private lateinit var mAuth: FirebaseAuth
-    private lateinit var mAuthListener: AuthStateListener
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+    private lateinit var mFireBaseDatabase: FirebaseDatabase
+    private lateinit var mDatabaseReference: DatabaseReference
     private lateinit var mUserDatabaseReference: DatabaseReference
-
+    val itemList1: ArrayList<String> = ArrayList()
+    val itemList2: ArrayList<String> = ArrayList()
     private lateinit var mDrawerLayout: DrawerLayout
     private lateinit var mDrawerToggleButton: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_colours)//TODO 4
 
         mAuth = FirebaseAuth.getInstance()
     }
 
     public override fun onStart() {
         super.onStart()
-        mAuthListener = AuthStateListener { firebaseAuth ->
+        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
             val user = firebaseAuth.currentUser
-            if (user == null ) {
+            if (user == null) {
                 val intent = Intent(applicationContext, LoginActivity::class.java)
                 startActivity(intent)
                 finish()
-            }
-            else {
-                mDrawerLayout = findViewById(R.id.drawer_layout)
+            } else {
+
+                mDrawerLayout = findViewById(R.id.drawer_layout_colours)//TODO 1
                 mDrawerToggleButton = ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close)
                 mDrawerLayout.addDrawerListener(mDrawerToggleButton)
                 mDrawerToggleButton.syncState()
 
                 supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-                val navigationView = findViewById<NavigationView>(R.id.nav_view)
+                val navigationView = findViewById<NavigationView>(R.id.nav_view_colours)//TODO 2
                 val menu = navigationView.menu
                 val userName = menu.findItem(R.id.nav_username)
+
                 userName.title = "Welcome, "+user.displayName.toString()
 
                 mUserDatabaseReference = FirebaseDatabase.getInstance().getReference("Users").child(user.uid)
@@ -77,49 +82,46 @@ class MainActivity : NavigationView.OnNavigationItemSelectedListener, AppCompatA
 
                 navigationView.setNavigationItemSelectedListener(this)
 
-                alphabets.setOnClickListener {
-                    val intent = Intent(applicationContext, AlphabetsActivity::class.java)
-                    startActivity(intent)
-                }
+                mFireBaseDatabase = FirebaseDatabase.getInstance()
+                mDatabaseReference = mFireBaseDatabase.getReference("Colours")
 
-                numbers.setOnClickListener {
-                    val intent = Intent(applicationContext, NumbersActivity::class.java)
-                    startActivity(intent)
-                }
+                colour_recycler_list.layoutManager = LinearLayoutManager(this)
+                colour_recycler_list.adapter = TwoColRecyclerAdapter(itemList1, itemList2, this)
 
-                days.setOnClickListener {
-                    val intent = Intent(applicationContext, DaysOfWeekActivity::class.java)
-                    startActivity(intent)
-                }
+                mDatabaseReference.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
 
-                colours.setOnClickListener {
-                    val intent = Intent(applicationContext, ColoursActivity::class.java)
-                    startActivity(intent)
-                }
-                test_textView.setOnClickListener {
-                    val intent = Intent(applicationContext, TestActivity::class.java)
-                    startActivity(intent)
-                }
+                        for(dataSnapshot1: DataSnapshot in dataSnapshot.children){
+                            val value = dataSnapshot1.getValue(Colours::class.java)
+                            itemList1.add(value!!.englishColour)
+                            itemList2.add(value.germanColour)
+                        }
+                        colour_recycler_list.adapter!!.notifyDataSetChanged()
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        Toast.makeText(applicationContext, "Can't fetch data", Toast.LENGTH_SHORT).show()
+                    }
+                })
             }
         }
     }
 
     public override fun onResume() {
         super.onResume()
+        itemList1.clear()
+        itemList2.clear()
+
         mAuth.addAuthStateListener(mAuthListener)
     }
 
     public override fun onStop() {
         super.onStop()
-        if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener)
-        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
         when (item.itemId) {
-
             R.id.logout -> {
                 FirebaseAuth.getInstance().signOut()
             }
