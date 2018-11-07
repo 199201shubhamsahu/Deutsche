96826cdabc3923251c85853aa00031f98187da57
pizzasahu <199201shubhamsahu@gmail.com>
2018-11-07 14:11:34 +0530

    TextView Changes

--- app/src/main/java/com/example/root/deutsche/MainActivity.kt
+++ app/src/main/java/com/example/root/deutsche/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.root.deutsche
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import android.support.design.widget.NavigationView
 import com.google.firebase.auth.FirebaseAuth.AuthStateListener
